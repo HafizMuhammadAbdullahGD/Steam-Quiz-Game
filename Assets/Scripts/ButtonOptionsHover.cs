8bace549509abef84ca15fd1217f8934b2a9a3a2
Haiz Muhammad Abdullah <hafizmuhammadabdullah95@gmail.com>
2024-07-05 10:43:36 +0500

    Bug Fixed

--- Assets/Scripts/ButtonOptionsHover.cs
+++ Assets/Scripts/ButtonOptionsHover.cs
@@ -1,49 +1,10 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
-using System.Collections.Generic;
-using System;
-using Unity.VisualScripting;
+
 
 public class ButtonOptionsHover : MonoBehaviour
 {
-    //     GraphicRaycaster m_Raycaster;
-    //     PointerEventData m_PointerEventData;
-    //     EventSystem m_EventSystem;
 
-    //     void Start()
-    //     {
-    //         //Fetch the Raycaster from the GameObject (the Canvas)
-    //         m_Raycaster = GetComponent<GraphicRaycaster>();
-    //         //Fetch the Event System from the Scene
-    //         m_EventSystem = GetComponent<EventSystem>();
-    //     }
-    //     public void Entered()
-    //     {
-    //         print(1);
-    //     }
-    //     void Update()
-    //     {
-    //         return;
-    //         // //Check if the left Mouse button is clicked
-
-    //         // //Set up the new Pointer Event
-    //         // m_PointerEventData = new PointerEventData(m_EventSystem);
-    //         // //Set the Pointer Event Position to that of the mouse position
-    //         // m_PointerEventData.position = new Vector2(Screen.width / 2, Screen.height / 2);
-
-    //         // //Create a list of Raycast Results
-    //         // List<RaycastResult> results = new List<RaycastResult>();
-
-    //         // //Raycast using the Graphics Raycaster and mouse click position
-    //         // m_Raycaster.Raycast(m_PointerEventData, results);
-
-    //         // //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
-    //         // foreach (RaycastResult result in results)
-    //         // {
-    //         //     Debug.Log("Hit " + result.gameObject.name);
-    //         // }
-    //     }
     Button BtncurrHit;
     Button BtnprevHit;
     private void Start()
--- Assets/Scripts/Managers/PlayerSpawnManager.cs
+++ Assets/Scripts/Managers/PlayerSpawnManager.cs
@@ -1,14 +1,5 @@
 using System;
 using UnityEngine;
-// [Serializable]
-// public struct PlayerDataToSpawn
-// {
-//     public Transform CameraMain;
-//     public Transform AvatarPlayyer;
-//     public Transform CameraCinemachine;
-//     public Transform CanvasPlayer;
-
-// }
 public class PlayerSpawnManager : MonoBehaviour
 {
     [SerializeField] private Transform[] playersToSpawn;
@@ -24,10 +15,7 @@ public class PlayerSpawnManager : MonoBehaviour
     }
     private void OnPlayerSpawnCalled(int ind)
     {
-        // Instantiate(playerDataToSpawns[ind].CameraMain);
-        // Instantiate(playerDataToSpawns[ind].AvatarPlayyer);
-        // Instantiate(playerDataToSpawns[ind].CameraCinemachine);
-        // Instantiate(playerDataToSpawns[ind].CanvasPlayer);
+
         quizLevel.gameObject.SetActive(true);
         playersToSpawn[ind].gameObject.SetActive(true);
         Cursor.visible = false;
